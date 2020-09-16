f3b030189241d126cb131e1c28f3858f277f7fdd
ZFW <2515526134@QQ.COM>
2020-09-16 15:38:49 +0800

    20200916 15:37

---
+++ zipkincilent/src/main/java/com/zipkincilent/ZipkincilentApplication.java
@@ -0,0 +1,13 @@
+package com.zipkincilent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipkincilentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkincilentApplication.class, args);
+    }
+
+}
---
+++ zipkincilent/src/test/java/com/zipkincilent/ZipkincilentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zipkincilent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipkincilentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
